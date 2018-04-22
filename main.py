a191f1023307727e4eb7256cb2deff13d7ff1414
Cyan Tarek <cyantarek.cg@gmail.com>
2018-04-22 22:13:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import os
 
+first_dir = os.listdir('.')
+second_dir = os.listdir(r'second_dir_path')
+
+for f in first_dir:
+    if f in second_dir:
+        pass
+    else:
+        print(f)
+    
